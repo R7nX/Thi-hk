92e1f373c71dc1ff875d4c7e3ad319f4a6bba573
R7nX <101917945+R7nX@users.noreply.github.com>
2022-04-21 19:23:07 +0700

    Add files via upload
    test, check to see if can open

---
+++ Final/Final.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+float pay(float a){
+    a = a*27578;
+    return a;
+
+}
+
+
+int main(){
+
+int n;
+freopen("Input.txt", "r", stdin);
+cin >> n;
+int a[7];
+
+cout << "test array: ";
+for(int i = 0; i <= 6; i++){
+    cin >> a[i];
+    cout << a[i] << " ";
+
+    
+}
+
+
+
+
+    return 0; 
+}
